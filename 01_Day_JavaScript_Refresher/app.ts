a2a70eb2897eaccc8a8470cde9e791842a038661
Casian Leonte <leontecasian03@yahoo.ro>
2025-07-02 23:39:22 +0200

    excercise 1 done

---
+++ 01_Day_JavaScript_Refresher/app.ts
@@ -0,0 +1,39 @@
+// Excercises: Level 1
+// 1)
+function fullName(firstName: string, lastName: string): string { 
+    return `${firstName} ${lastName}`;
+}
+
+// 2)
+function addNumbers(a: number, b: number): number{
+    return a + b;
+}
+
+// 3)
+function areaCircle(r: number): number{
+    return 3.14 * r * r;
+}
+
+// 4)
+function celciusToFahr(deg: number): number{
+    return deg * (9/5) + 32;
+}
+
+// 5)
+function bmiCalc(weightKg: number, height: number): string{
+    let bmi: number = weightKg / (height * height)
+    
+    if(bmi < 18.5){
+        return `Underweight with BMI of ${bmi}`;
+    }
+    else if(bmi >= 18.5 && bmi < 24.9){
+        return `Normal weight with BMI of ${bmi}`;
+    }
+    else if(bmi >= 24.9 && bmi < 25){
+        return `Overweight with BMI of ${bmi}`;
+    }
+    else{
+        return `Obese with BMI of ${bmi}`; 
+    }
+}
+
